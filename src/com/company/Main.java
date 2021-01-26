6550dcd0fba06a8183fa4b2034ab5eb556c89a4e
aidanatm <temirkulovaa2005@gmail.com>
2021-01-26 16:05:05 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,42 @@
+package com.company;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<String>
+                listA = new ArrayList<>(),
+                listB = new ArrayList<>(),
+                listC = new ArrayList<>();
+
+        arrayStringFilling(listA);
+        System.out.println(listA + " listA");
+
+        arrayStringFilling(listB);
+        System.out.println(listB + " listB");
+
+        Collections.reverse(listB);
+
+        Iterator <String>
+                iteratorListA = listA.iterator(),
+                iteratorListB = listB.iterator();
+
+        while (iteratorListA.hasNext() && iteratorListB.hasNext()){
+            listC.add(iteratorListA.next());
+            listC.add(iteratorListB.next());
+        }
+
+        System.out.println(listC + " listC");
+    }
+
+    public static void arrayStringFilling(Collection<String> list) {
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Введите строку");
+            String s = scanner.nextLine();
+            list.add(s);
+        }
+    }
+
+}
